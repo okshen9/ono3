7dcb6019444691e1c9edd90ea4591f4a9b92fa16
okshen9 <dev@minimal.localdomain>
2017-12-14 14:14:38 +0300

    ljgbsrlkhsf

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.TextChanged += new EventHandler(textBox1_TextChanged);
+            textBox2.TextChanged += new EventHandler(textBox2_TextChanged);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox2.Text = textBox1.Text; 
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox2.Text;
+        }
+    }
+}
